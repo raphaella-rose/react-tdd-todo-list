eb15ec86f23b743500d7384c1dc4cc0447ea8712
raphaella-rose <raphaella.rose1@gmail.com>
2022-07-29 17:11:02 +0100

    page has title "Todo List"

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const TodoList = () => {
-  return <div data-testid="TodoList"></div>
+  return (
+  <div data-testid="TodoList">
+    <h1>Todo List</h1>
+  </div>
+  )
 }
 
 export default TodoList;
--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -3,9 +3,15 @@ import React from "react";
 import { render } from '@testing-library/react';
 import TodoList from "./TodoList";
 
+
 describe('TodoList Component', () => {
   it('should render', () => {
     const {getByTestId} = render(<TodoList />);
     expect(getByTestId('TodoList')).toBeInTheDocument();
+  })
+
+  it('should have a title', () => {
+    const {getByText} = render(<TodoList />);
+    expect(getByText('Todo List')).toBeInTheDocument();
   })
 })
